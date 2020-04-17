7453c32c1c3a248b0934a94fad8b63ef9f2291c7
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-17 20:07:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(integer)
+  if (integer == 0)
+    return false
+  elsif (integer % 2 == 0)
+    return true
+  elsif (integer / integer == 0)
+      return true
+  elsif (integer % 2 != 0)
+    return false
+  end
+end
