4bc2fe782d3ccf1aba2ba0907fa3350e28886ae8
Dan Ste. Marie Jr <dste.mariejr@gmail.com>
2020-04-17 20:08:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,29 @@
-# Add  code here!
+# # Add  code here!
+# def prime?(integer)
+#   if (integer == 0)
+#     return false
+#   elsif (integer % 2 == 0)
+#     return true
+#   elsif (integer / integer == 0)
+#       return true
+#   elsif (integer % 2 != 0)
+#     return false
+#   end
+# end
+
 def prime?(integer)
-  if (integer == 0)
-    return false
-  elsif (integer % 2 == 0)
-    return true
-  elsif (integer / integer == 0)
+    time = []
+    counter = integer - 1
+    while counter != 0
+      integer/counter
+      if integer % counter == 0
+        time << counter
+        counter -= 1
+      end
+    end
+    if time.length == 1
       return true
-  elsif (integer % 2 != 0)
-    return false
-  end
+    elsif time.length != 1
+      return false
+    end
 end
